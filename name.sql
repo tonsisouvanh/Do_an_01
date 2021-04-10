332157eaebaaa65f4febfa8bb76c078d9ca404e7
Tons <tonsisouvanh7@gmail.com>
2021-04-10 12:02:55 +0700

    added sql file

---
+++ name.sql
@@ -0,0 +1,2 @@
+insert into BENHNHAN (maBN, hoTen, ngaySinh, diaChi, sdt) 
+values (1, 'Lê Duy Minh', TO_DATE('12/01/1984', 'DD/MM/YYYY'),'62 Ph? Ð?u Ð?i Th?, Thôn Nhu?ng Bình, Qu?n Tài, HCM','0755586591');
